44d6cd23e655b0df0d352ebf981565484ba16842
YujiSoftware <yuji.software+github@gmail.com>
2022-09-21 00:05:03 +0900

    Add MessageBox sample

---
+++ MessageBox.java
@@ -0,0 +1,39 @@
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+import java.nio.charset.StandardCharsets;
+
+import static java.lang.foreign.ValueLayout.*;
+
+public class MessageBox {
+    private static final int MB_ICONINFORMATION = 0x00000040;
+    private static final int MB_OKCANCEL = 0x00000001;
+
+    public static MemorySegment allocateUtf16String(String str, SegmentAllocator allocator) {
+
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_16LE);
+        MemorySegment addr = allocator.allocate(bytes.length + 1);
+
+        MemorySegment segment = MemorySegment.ofArray(bytes);
+        addr.copyFrom(segment);
+        addr.set(JAVA_BYTE, bytes.length, (byte) 0);
+
+        return addr;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Linker linker = Linker.nativeLinker();
+        try (MemorySession session = MemorySession.openConfined()) {
+            SymbolLookup lookup = SymbolLookup.libraryLookup("User32", session);
+            MethodHandle messageBox = linker.downcallHandle(
+                    lookup.lookup("MessageBoxW").get(),
+                    FunctionDescriptor.of(JAVA_INT, JAVA_LONG, ADDRESS, ADDRESS, JAVA_INT));
+
+            MemorySegment text = allocateUtf16String("こんにちは！", session);
+            MemorySegment caption = allocateUtf16String("Java", session);
+
+            int ret = (int) messageBox.invoke(0, text, caption, MB_OKCANCEL | MB_ICONINFORMATION);
+
+            System.out.println(ret);
+        }
+    }
+}
---
+++ MessageBoxA.java
@@ -0,0 +1,26 @@
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+
+import static java.lang.foreign.ValueLayout.*;
+
+public class MessageBoxA {
+    private static final int MB_ICONINFORMATION = 0x00000040;
+    private static final int MB_OKCANCEL = 0x00000001;
+
+    public static void main(String[] args) throws Throwable {
+        Linker linker = Linker.nativeLinker();
+        try (MemorySession session = MemorySession.openConfined()) {
+            SymbolLookup lookup = SymbolLookup.libraryLookup("User32", session);
+            MethodHandle messageBox = linker.downcallHandle(
+                    lookup.lookup("MessageBoxA").get(),
+                    FunctionDescriptor.of(JAVA_INT, JAVA_LONG, ADDRESS, ADDRESS, JAVA_INT));
+
+            MemorySegment text = session.allocateUtf8String("Hello world.");
+            MemorySegment caption = session.allocateUtf8String("Java");
+
+            int ret = (int) messageBox.invoke(0, text, caption, MB_OKCANCEL | MB_ICONINFORMATION);
+
+            System.out.println(ret);
+        }
+    }
+}
